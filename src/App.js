769751863cd1c33dfcc99863101b8a35a0fe53ea
Marlon Leon <ghostveene@gmail.com>
2023-03-30 12:21:16 -0400

    added startgame and end game function and fixed time and typing bug

--- src/App.js
+++ src/App.js
@@ -1,36 +1,62 @@
 import React from "react"
 
 export default function App() {
+	const STARTING_TIME = 5
+
 	const [text, setText] = React.useState("")
-	const [timeRemaining, setTimeRemaining] = React.useState(5)
-	const [gameRunning, setGameRunning] = React.useState(false)
+	const [timeRemaining, setTimeRemaining] = React.useState(STARTING_TIME)
+	const [isGameRunning, setIsGameRunning] = React.useState(false)
+	const [wordCount, setWordCount] = React.useState(0)
 
 	React.useEffect(() => {
-		if (timeRemaining > 0) {
-			setTimeout(() => {
-				setTimeRemaining(time => time - 1)
-			}, 1000)
+		if (isGameRunning) {
+			if (timeRemaining > 0) {
+				setTimeout(() => {
+					setTimeRemaining(time => time - 1)
+				}, 1000)
+			} else if (timeRemaining === 0) {
+				endGame()
+			}
 		}
-	}, [timeRemaining])
+	}, [timeRemaining, isGameRunning])
+
+	function startGame() {
+			setIsGameRunning(true)
+			setTimeRemaining(STARTING_TIME)
+			setText("")
+	}
+
+	function endGame() {
+		setIsGameRunning(false)
+		calculateWordCount(text)
+	}
 
 	function handleChange(event) {
 		setText(event.target.value)
 	}
-
+	
 	function calculateWordCount(text) {
-		setGameRunning(true)
 		const wordsArr = text.trim().split(" ")
 		const filteredWords = wordsArr.filter(word => word !== "")
-		return filteredWords.length
+		setWordCount(filteredWords.length)
 	}
 
 	return (
 		<div>
 			<h1>How fast do you type?</h1>
-			<textarea value={text} onChange={handleChange}/>
+			<textarea 
+				value={text} 
+				onChange={handleChange} 
+				disabled={isGameRunning === false}
+			/>
 			<h4>Time Remaing: {timeRemaining}</h4>
-			<button onClick={() => calculateWordCount(text)}>Start Game</button>
-			<h1>Word Count: ???</h1>
+			<button 
+				onClick={startGame} 
+				disabled={isGameRunning}
+			>
+				Start Game
+			</button>
+			<h1>Word Count: {wordCount}</h1>
 		</div>
 	)
 }
