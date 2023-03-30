8d6bb42303c88bafd59d99b3414087c52ac24bf8
Marlon Leon <ghostveene@gmail.com>
2023-03-30 10:53:05 -0400

    setup state tracking text in textarea

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from "react"
+
 export default function App() {
+	const [text, setText] = React.useState("")
+
+	function handleChange(event) {
+		setText(event.target.value)
+	}
+
 	return (
-		<div></div>
+		<div>
+			<h1>How fast do you type?</h1>
+			<textarea value={text} onChange={handleChange}/>
+			<h4>Time Remaing: ???</h4>
+			<button>Start Game</button>
+			<h1>Word Count: ???</h1>
+		</div>
 	)
 }
