253b57b44d4180a1b94464df9bd97950c84869b3
Marlon Leon <ghostveene@gmail.com>
2023-03-30 14:09:14 -0400

    moved hooks to seperate file and imported to app

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
-import {useEffect, useRef, useState} from "react"
+import wordGame from "./hooks/wordGame"
 
 export default function App() {
-	const STARTING_TIME = 5
-
-	const [text, setText] = useState("")
-	const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-	const [isGameRunning, setIsGameRunning] = useState(false)
-	const [wordCount, setWordCount] = useState(0)
-	const textBoxRef = useRef(null)
-
-	useEffect(() => {
-		if (isGameRunning) {
-			if (timeRemaining > 0) {
-				setTimeout(() => {
-					setTimeRemaining(time => time - 1)
-				}, 1000)
-			} else if (timeRemaining === 0) {
-				endGame()
-			}
-		}
-	}, [timeRemaining, isGameRunning])
-
-	function startGame() {
-			setIsGameRunning(true)
-			setTimeRemaining(STARTING_TIME)
-			setText("")
-			textBoxRef.current.disabled = false
-			textBoxRef.current.focus()
-	}
-
-	function endGame() {
-		setIsGameRunning(false)
-		calculateWordCount(text)
-	}
-
-	function handleChange(event) {
-		setText(event.target.value)
-	}
+	const [
+        text, 
+        timeRemaining, 
+        isGameRunning, 
+        wordCount, 
+        textBoxRef, 
+        startGame,
+        handleChange
+    ] = wordGame()
 	
-	function calculateWordCount(text) {
-		const wordsArr = text.trim().split(" ")
-		const filteredWords = wordsArr.filter(word => word !== "")
-		setWordCount(filteredWords.length)
-	}
-
 	return (
 		<div>
 			<h1>How fast do you type?</h1>
